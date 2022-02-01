3cd31149d7bb16cb535b9a0e4d7f0f406a074e11
Alex <achekanov@exadel.com>
2022-02-01 10:21:29 +0100

    message

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lesson1
+{
+    //2. (constructor) Create class with 2 constructors and create 2 objects that initialize 2 ways
+    //internal class Program
+    class Movie
+    {
+        public string genre;
+        public string year;
+        public Movie()
+        {
+            genre = "Horror";
+            year =  ""
+        }
+        static void Main(string[] args)
+        {
+            Movie Midsommar = new Movie();
+            Console.WriteLine(Midsommar.genre);
+        }
+    }
+}
